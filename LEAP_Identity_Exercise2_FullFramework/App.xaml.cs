34807d37da78dbbe740265331fce5042ea834e2e
Hans Løken <hans@ebamse.net>
2017-05-22 14:35:54 +0200

    Working lab for desktop WPF app

---
+++ LEAP_Identity_Exercise2_FullFramework/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Microsoft.Identity.Client;
+
+namespace LEAP_Identity_Exercise2_FullFramework
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Below is the clientId of your app registration. 
+        //You have to replace the below with the Application Id for your app registration
+        private static string ClientId = "0c1f59bd-2b10-48ab-ae20-beca2330cf1f";
+
+        public static PublicClientApplication PublicClientApp = new PublicClientApplication(ClientId);
+
+    }
+}
